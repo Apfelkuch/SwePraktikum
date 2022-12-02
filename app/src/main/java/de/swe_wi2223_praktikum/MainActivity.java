59192a7df8efa89bfafca41fbdd3ba20c1573da2
Sandgeborener <felix.grigarek@gmail.com>
2022-12-02 16:05:05 +0100

    Fragmant versuchen von main aufzufrufen

--- app/src/main/java/de/swe_wi2223_praktikum/MainActivity.java
+++ app/src/main/java/de/swe_wi2223_praktikum/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.ClipData;
 
 import android.os.Bundle;
@@ -28,57 +30,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  {
 
-    ArrayList<Med> list = new ArrayList<>();
+    FragmentManager manager = getFragmentManager();
+    FragmentTransaction transaction = manager.beginTransaction();
+transaction.replace(R.id.container,YOUR_FRAGMENT_NAME,YOUR_FRAGMENT_STRING_TAG);
+transaction.addToBackStack(null);
+transaction.commit();
 
-    private RecyclerView recyclerView;
-    private MedAdapter adapter;
-
-    @SuppressLint("InflateParams")
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_main,container,false);
-
-        //Zuweisung der Objekte aus der activity_main.xml
-        recyclerView = view.findViewById(R.id.recyViewMed);
-        FloatingActionButton addMed = view.findViewById(R.id.addMed);
-
-        //Dient dem RecycleViewer, um eine vertikale Liste zu erstellen.
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
-
-        //Button zum shoppen
-        addMed.setOnClickListener(dialog_view -> {
-            //Baut das Dialogfenster auf
-            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-            dialog_view = getLayoutInflater().inflate(R.layout.add_med_dialog, null);
-
-            //Variablen, zum editieren aus der XML-Datei
-            EditText editMed = dialog_view.findViewById(R.id.editTextTextMed);
-            EditText editCount = dialog_view.findViewById(R.id.editTextNumber);
-            EditText editRecip = dialog_view.findViewById(R.id.editTextNumber2);
-
-            //Der Builder, um das Fenster zu tatäschlich zu befüllen
-            builder.setView(dialog_view)
-                    .setTitle("Shop")
-                    .setPositiveButton("Bestellen", (dialogInterface, i) -> {
-                        String tmpMed = editMed.getText().toString();
-                        String tmpCount = editCount.getText().toString();
-                        String tmpRecip = editRecip.getText().toString();
-
-                        //Am Ende hinzufügen, Software notifyen damit sie animiert, View springt zum Eintrag am Ende.
-                        list.add(new Med(tmpMed, tmpCount, tmpRecip));
-                        adapter.notifyItemInserted(list.size()-1);
-                        recyclerView.scrollToPosition(list.size()-1);
-                    })
-                    .setNegativeButton("Abbrechen", (dialogInterface, i) -> {
-                        //Der Cancel-Button darf leer sein, da als "Default" alles abgebrochen wird und keine Änderungen stattfinden.
-                    });
-            builder.show();
-        });
-
-        adapter = new MedAdapter(requireActivity(), list);
-        recyclerView.setAdapter(adapter);
-
-        return view;
-    }
 }
